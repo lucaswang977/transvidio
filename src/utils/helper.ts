b534221847950e3ce2abb4d11d40446adb04dd29
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2023-07-08 16:26:44 +0900

    fix bug [#20]

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -9,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function timeFormat(milliseconds: number) {
   const minutes = Math.floor(milliseconds / 1000 / 60).toString().padStart(2, '0');
-  const seconds = Math.floor(milliseconds / 1000 % 3600).toString().padStart(2, '0');
+  const seconds = Math.floor(milliseconds / 1000 % 60).toString().padStart(2, '0');
   const millisecondsFormatted = Math.floor(milliseconds % 1000 / 100).toString().padStart(1, '0');
 
   return `${minutes}:${seconds}.${millisecondsFormatted}`;
