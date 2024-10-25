b01e82686e9fcac10d6930ed5672cc1299b402ed
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2024-10-25 08:03:46 +0000

    PostgreSQL added

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,3 +2,6 @@
 source ~/.zshrc
 omz theme set simple
 omz plugin enable vi-mode
+
+sudo service postgresql start
+psql -U postgres -h 127.0.0.1 -c "CREATE ROLE puser LOGIN password 'localdbpassword';" -c "CREATE DATABASE testdb1 ENCODING 'UTF8' OWNER puser TEMPLATE template0;"
