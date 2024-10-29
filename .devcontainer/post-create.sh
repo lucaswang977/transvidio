44cef6b9ede85777418f31c1d403446a3b2cf9e4
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2024-10-29 02:25:18 +0000

    Fix db creation sql problem.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,6 +4,9 @@ omz theme set simple
 omz plugin enable vi-mode
 
 sudo service postgresql start
-psql -U postgres -h 127.0.0.1 \
--c "DO \$\$ BEGIN IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'puser') THEN CREATE ROLE puser WITH LOGIN PASSWORD 'localdbpassword'; END IF; END \$\$;" \
--c "DO \$\$ BEGIN IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testdb1') THEN CREATE DATABASE testdb1 ENCODING 'UTF8' OWNER puser TEMPLATE template0; END IF; END \$\$;"
+psql -U postgres -h 127.0.0.1 -c "DO \$\$ BEGIN IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'puser') THEN CREATE ROLE puser WITH LOGIN PASSWORD 'localdbpassword'; END IF; END \$\$;"
+if ! psql -U postgres -h 127.0.0.1 -tc "SELECT 1 FROM pg_database WHERE datname = 'testdb1'" | grep -q 1; then
+    psql -U postgres -h 127.0.0.1 -c "CREATE DATABASE testdb1 ENCODING 'UTF8' OWNER puser TEMPLATE template0;"
+else
+    echo "Database 'testdb1' already exists."
+fi
